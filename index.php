de332c82266d31a76b776db4871ba0c9fac17b36
Margad Gantuya <majagantuya@gmail.com>
2022-06-13 12:07:31 +0200

    create router

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-echo 'hello';
+<?php 
+session_start();
+
+require 'helpers.php';
+
+
+spl_autoload_register(function ($className) {
+    $fileName = str_replace('\\', '/', $className);
+    require "src/$fileName.php";
+});
+
+$routes = require 'config/routes.php';
+
+if (isset($routes[$route])) {
+    list($controllerName, $method) = $routes[$route];
+
+    $controller = new $controllerName();
+    $controller->$method();
+} else {
+    echo 'bug';
+}
